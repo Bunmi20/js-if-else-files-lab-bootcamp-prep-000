bc464dc98524cf3302b58c3a3a37ce9cb5f95ad6
olubunmi oluwashola <olubunmioluwashola47@gmail.com>
2019-04-28 23:38:39 +0000

    Automatically backed up by Learn

--- tests.js
+++ tests.js
@@ -1,15 +1,3 @@
-var index = require("./index.js")
+Expected: Susan, Received: joe)
+Expected: 70, Received: 74)
 
-if (index.name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected: Susan, Received: "+index.name)
-}
-
-
-
-if (index.height === 70) {
-    console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
-}
